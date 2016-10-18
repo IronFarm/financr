179b2416cbde43a580829d05a119b936ab116aef
Carl Jeske <carljeske@googlemail.com>
2016-10-18 19:56:41 +0100

    Factor out all HL integration into a separate module
    * Major tidy up
    * Account data stored in more useful format

--- financr/funds.py
+++ financr/funds.py
@@ -7,76 +7,20 @@ import requests
 from bokeh.charts import Line, output_file, show
 from lxml import html
 
+import hl
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 username = config.get('HL', 'username')
 dob = config.get('HL', 'dob')
-pwd = config.get('HL', 'pwd')
-
-login_url1 = 'https://online.hl.co.uk/my-accounts/login-step-one'
-login_url2 = 'https://online.hl.co.uk/my-accounts/login-step-two'
-account_summary_url = 'https://online.hl.co.uk/my-accounts/account_summary/account/22'
+password = config.get('HL', 'password')
 
 price_history_url = 'http://markets.ft.com/data/funds/tearsheet/historical'
 
 
-def get_validation_token(content):
-    parsed_html = html.fromstring(content)
-    validation_token = parsed_html.xpath("//input[@name='hl_vt']/@value")
-
-    return validation_token
-
-
-def get_list_of_holdings(account_summary):
-    parsed_html = html.fromstring(account_summary)
-
-    holding_names = [x.text for x in parsed_html.xpath('//*[@id="holdings-table"]/tbody/tr/td/div/a/span')]
-    transaction_urls = [x.attrib['href'] for x in parsed_html.xpath('//*[@id="holdings-table"]/tbody/tr/td[1]/*')]
-    detail_urls = [x.attrib['href'].replace('security_details', 'fund_key_features')
-                   for x in parsed_html.xpath('//*[@id="holdings-table"]/tbody/tr/td/div/a[@class="factsheet-button"]')]
-    return holding_names, transaction_urls, detail_urls
-
-
-def get_transaction_history_for_url(session, name, url):
-    print 'Downloading transaction history for {}'.format(name)
-    transaction_rows = './/*[@id="movements-table-container"]/table/tbody/tr'
-    data = []
-
-    result = session.get(url)
-    parsed_html = html.fromstring(result.content)
-
-    for row in parsed_html.findall(transaction_rows):
-        data.append((name, ) + tuple(x.text.strip() for x in row.findall('td')))
-
-    return data
-
-
-def extract_isin_from_url(session, url):
-    isin_path = './/*[@id="security-factsheet"]/div/div[7]/div[2]/div/div/table/tbody/tr[th="ISIN code:"]/td'
-
-    result = session.get(url)
-    parsed_html = html.fromstring(result.content)
-
-    isin = parsed_html.find(isin_path).text.strip()
-
-    return isin
-
-
-def create_transaction_history(transaction_data):
-    transaction_history = pd.DataFrame.from_records(
-        transaction_data, exclude=['record'], columns=['fund', 'date', 'type', 'record', 'fund_price', 'units', 'cost']
-    )
-    transaction_history['date'] = pd.to_datetime(transaction_history['date'], dayfirst=True)
-    transaction_history['fund_price'] = transaction_history['fund_price'].apply(lambda x: float(x.replace(',', '')))
-    transaction_history['units'] = transaction_history['units'].apply(lambda x: float(x.replace(',', '')))
-    transaction_history['cost'] = transaction_history['cost'].apply(lambda x: float(x.replace(',', '')))
-    transaction_history = transaction_history.set_index(['date','fund']).sort_index()
-
-    return transaction_history
-
-
 def get_fund_price_history(name, fund_isin, start_date):
+
     print 'Downloading price history for {}'.format(name)
     row_xpath = ('.//*[@class="mod-ui-table mod-tearsheet-historical-prices__results mod-ui-table--freeze-pane"]'
                  '/tbody/tr')
@@ -106,9 +50,9 @@ def get_fund_price_history(name, fund_isin, start_date):
         price_history.append((date, name, price))
 
     while price_history[-1][0] > start_date:
-        extra_results = my_session.get('http://markets.ft.com/data/equities/ajax/getmorehistoricalprices',
-                                       params={'resultsStartDate': next_date, 'symbol': symbol,
-                                               'isLastRowStriped': 'false'})
+        extra_results = requests.get('http://markets.ft.com/data/equities/ajax/getmorehistoricalprices',
+                                     params={'resultsStartDate': next_date, 'symbol': symbol,
+                                             'isLastRowStriped': 'false'})
 
         next_date = extra_results.json()['data']['startDate']
 
@@ -124,40 +68,14 @@ def get_fund_price_history(name, fund_isin, start_date):
 
 
 if __name__ == '__main__':
-    my_session = requests.session()
-
-    result = my_session.get(login_url1)
-    vt = get_validation_token(result.content)
-
-    result = my_session.post(login_url1, {'hl_vt': vt, 'username': username, 'DoB': dob, 'submit.x': '50',
-                                          'submit.y': '20', 'submit': 'login'})
-
-    result = my_session.get(login_url2)
-    vt = get_validation_token(result.content)
-    parsed_html = html.fromstring(result.content)
-    required_chars = [int(x.text) - 1 for x in parsed_html.xpath('//*[@id="login-box-border"]/div/p/strong')]
-
-    result = my_session.post(login_url2, {'hl_vt': vt, 'pChar1': pwd[required_chars[0]],
-                                          'pChar2': pwd[required_chars[1]], 'pChar3': pwd[required_chars[2]],
-                                          'submit.x': '50', 'submit.y': '20', 'submit': 'login'})
-    result = my_session.get(account_summary_url)
-
-    holdings_data = get_list_of_holdings(result.content)
-    all_transactions = []
-    fund_isins = []
-    for name, url, isin_url in zip(holdings_data[0], holdings_data[1], holdings_data[2]):
-        history = get_transaction_history_for_url(my_session, name, url)
-        all_transactions.extend(history)
-
-        fund_isins.append((name, extract_isin_from_url(my_session, isin_url)))
-
-    transaction_history = create_transaction_history(all_transactions)
+    account_data = hl.download_account_data(username, dob, password)
+    transaction_history = hl.create_transaction_history(account_data)
     total_holdings = transaction_history.groupby(level='fund')[['units', 'cost']].cumsum()
 
     price_data = []
-    for name, isin in fund_isins:
-        start_date = total_holdings.loc(axis=0)[:, name].index.values[0][0]
-        price_data.extend(get_fund_price_history(name, isin, start_date))
+    for holding in account_data:
+        start_date = total_holdings.loc(axis=0)[:, holding['name']].index.values[0][0]
+        price_data.extend(get_fund_price_history(holding['name'], holding['isin'], start_date))
 
     price_history = pd.DataFrame.from_records(price_data, columns=['date', 'fund', 'fund_price'])
     price_history['date'] = pd.to_datetime(price_history['date'], dayfirst=True)
@@ -172,15 +90,15 @@ if __name__ == '__main__':
     value_history['profit'] = value_history.eval('value - cost')
 
     value_plot = Line(value_history, y='value', plot_width=1200, plot_height=600)
-    output_file("total_value.html")
+    output_file('total_value.html')
     show(value_plot)
 
     ratio_plot = Line(value_history, y='ratio', plot_width=1200, plot_height=600)
-    output_file("ratio.html")
+    output_file('ratio.html')
     show(ratio_plot)
 
     profit_plot = Line(value_history, y='profit', plot_width=1200, plot_height=600)
-    output_file("profit.html")
+    output_file('profit.html')
     show(profit_plot)
 
     print 'exit'
---
+++ financr/hl.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import requests
+from lxml import html
+
+LOGIN_STEP_ONE_URL = 'https://online.hl.co.uk/my-accounts/login-step-one'
+LOGIN_STEP_TWO_URL = 'https://online.hl.co.uk/my-accounts/login-step-two'
+ACCOUNT_SUMMARY_URL = 'https://online.hl.co.uk/my-accounts/account_summary/account/22'
+
+
+def find_validation_token(content):
+    parsed_html = html.fromstring(content)
+    validation_token = parsed_html.find('.//input[@name="hl_vt"]').attrib['value']
+
+    return validation_token
+
+
+def login_session(session, username, dob, password):
+    result = session.get(LOGIN_STEP_ONE_URL)
+    validation_token = find_validation_token(result.content)
+    session.post(LOGIN_STEP_ONE_URL, {'hl_vt': validation_token, 'username': username, 'DoB': dob, 'submit.x': '50',
+                                      'submit.y': '20', 'submit': 'login'})
+
+    result = session.get(LOGIN_STEP_TWO_URL)
+    validation_token = find_validation_token(result.content)
+    parsed_html = html.fromstring(result.content)
+    required_chars = [int(x.text) - 1 for x in parsed_html.findall('.//*[@id="login-box-border"]/div/p/strong')]
+    session.post(LOGIN_STEP_TWO_URL, {'hl_vt': validation_token, 'pChar1': password[required_chars[0]],
+                                      'pChar2': password[required_chars[1]], 'pChar3': password[required_chars[2]],
+                                      'submit.x': '50', 'submit.y': '20', 'submit': 'login'})
+
+
+def get_list_of_holdings(session):
+    result = session.get(ACCOUNT_SUMMARY_URL)
+    parsed_html = html.fromstring(result.content)
+
+    holdings = []
+    for row in parsed_html.findall('.//*[@id="holdings-table"]/tbody/tr'):
+        holding = {
+            'name': row.find('td/div/a/span').text,
+            'transaction_url': row.find('td[1]/a').attrib['href'],
+            'detail_url': row.find('.//*[@class="factsheet-button"]').attrib['href'].replace('security_details',
+                                                                                             'fund_key_features')
+        }
+        holdings.append(holding)
+
+    return holdings
+
+
+def download_transaction_history(session, name, url):
+    print 'Downloading transaction history for {}'.format(name)
+
+    transaction_rows = './/*[@id="movements-table-container"]/table/tbody/tr'
+    transaction_history = []
+
+    result = session.get(url)
+    parsed_html = html.fromstring(result.content)
+
+    for row in parsed_html.findall(transaction_rows):
+        transaction_history.append(tuple(x.text.strip() for x in row.findall('td')))
+
+    return transaction_history
+
+
+def get_isin_from_url(session, url):
+    result = session.get(url)
+    parsed_html = html.fromstring(result.content)
+
+    isin = parsed_html.find('.//*[th="ISIN code:"]/td').text.strip()
+
+    return isin
+
+
+def get_account_data_for_holdings(my_session, holdings):
+    account_data = []
+    for holding in holdings:
+        account = {
+            'name': holding['name'],
+            'transaction_history': download_transaction_history(my_session, holding['name'],
+                                                                holding['transaction_url']),
+            'isin': get_isin_from_url(my_session, holding['detail_url'])
+        }
+        account_data.append(account)
+
+    return account_data
+
+
+def download_account_data(username, dob, password):
+    my_session = requests.session()
+
+    login_session(my_session, username, dob, password)
+    holdings = get_list_of_holdings(my_session)
+    account_data = get_account_data_for_holdings(my_session, holdings)
+
+    return account_data
+
+
+def create_transaction_history(account_data):
+    transactions = []
+    for holding in account_data:
+        for transaction in holding['transaction_history']:
+            transactions.append((holding['name'], ) + transaction)
+
+    transaction_history = pd.DataFrame.from_records(
+        transactions, exclude=['record'], columns=['fund', 'date', 'type', 'record', 'fund_price', 'units', 'cost']
+    )
+    transaction_history['date'] = pd.to_datetime(transaction_history['date'], dayfirst=True)
+    transaction_history['fund_price'] = transaction_history['fund_price'].apply(lambda x: float(x.replace(',', '')))
+    transaction_history['units'] = transaction_history['units'].apply(lambda x: float(x.replace(',', '')))
+    transaction_history['cost'] = transaction_history['cost'].apply(lambda x: float(x.replace(',', '')))
+    transaction_history = transaction_history.set_index(['date','fund']).sort_index()
+
+    return transaction_history
+
